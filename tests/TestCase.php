310a5326de1282b9cbdc1eaa6a5dd31169f087cf
Eduardo Agostini <edu.agostini@gmail.com>
2018-07-19 11:32:07 +1200

    Composer
    Downgrading **PHP** version… 😥

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,7 @@ namespace Tests;
  * Class TestCase
  * @package Tests
  */
-abstract class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     //
 }
